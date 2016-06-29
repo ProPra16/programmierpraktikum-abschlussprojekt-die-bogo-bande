40e225dd6c62e81d7e0c43635e0df1574f4d2c2a
IWummi <sebis100@uni-duesseldorf.de>
2016-06-29 08:41:10 +0200

    added right E-mail

--- src/main/java/loader/LoadTest.java
+++ src/main/java/loader/LoadTest.java
@@ -1,1 +1,22 @@
+package loader;
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+
+
+public class LoadTest {
+    public static TextArea loaddata(TextArea texter,String s) {
+        final Path path = Paths.get(s + ".txt");
+        try {
+            texter.setText(String.valueOf(Files.readAllLines(path)));
+            System.out.println("Erfolgreich");
+        } catch (IOException e) {
+            System.out.println("Fehler beim Laden");
+            return null;
+        }
+        return texter;
+    }
+}
--- src/main/java/loader/SaveData.java
+++ src/main/java/loader/SaveData.java
@@ -1,1 +1,27 @@
+package loader;
+import javafx.scene.control.TextArea;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+
+public class SaveData {
+    public static void saveToTextFile(TextArea texter,String s) {
+        if (texter.getParagraphs() == null){
+            System.out.println("Keine Eingabe");
+        }
+        final Path path;
+        if(s.equals("code")) {
+            path = Paths.get("build/resources/main/code.txt");
+        }else {
+            path = Paths.get("build/resources/main/test.txt");
+        }
+        try {
+            Files.write(path,texter.getParagraphs());
+            System.out.println("Erfolgreich");
+        } catch (IOException e) {
+            System.out.println("Fehler beim schreiben");
+        }
+    }
+}
